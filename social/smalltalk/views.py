eea246e9418ae9d48bbf2b6c88726a743d20c73a
Rhuan Emanuel <rhnrod@gmail.com>
2025-05-30 00:28:03 -0300

    adjust front-end

--- social/smalltalk/views.py
+++ social/smalltalk/views.py
@@ -1,4 +1,29 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'home.html', {})
+def welcome(request):
+    context = {
+        'hide_header': True
+    }
+    return render(request, 'welcome.html', context)
+
+def login(request, slug=None):
+    context = {
+        'is_new_account': slug == 'new',
+        'hide_header': True
+    }
+    return render(request, 'login.html', context)
+
+def dashboard(request):
+    return render(request, 'dashboard.html', {})
+
+def profile(request, slug=None):
+    context = {
+        'profile' : slug
+    }
+    return render(request, 'profile.html', context)
+
+def search(request, slug=None):
+    context = {
+        'search_term' : slug
+    }
+    return render(request, 'search.html', context)
